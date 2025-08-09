04e62036201db53fbfad452286a2f455b2ce122a
andyss18 <its.andy.18d@gmail.com>
2025-08-09 02:17:44 -0600

    tarea 2 andy

---
+++ tarea 2 cadenas/TAREA2/src/tarea2/TAREA2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea2;
+
+/**
+ *
+ * @author itsan
+ */
+public class TAREA2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Crear la instancia de FRMMain (tu formulario)
+        FRMMAIN formulario = new FRMMAIN();
+        
+        // Hacer visible el formulario
+        formulario.setVisible(true);  // Muestra la ventana
+    }
+    
+}
